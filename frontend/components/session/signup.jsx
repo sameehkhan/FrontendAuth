4cc33f42bfc46e3d9409a0f1c77e16166aa22abb
Sameeh <sameehkhan1@gmail.com>
2018-12-07 09:44:27 -0800

    Create Sign up form

---
+++ frontend/components/session/signup.jsx
@@ -0,0 +1,9 @@
+import { connnect } from 'react-redux';
+import { createNewUser } from '../../actions/session';
+import Signup from './signup';
+
+const mapDispatchToProps = dispatch => ({
+    createNewUser: formUser => dispatch(createNewUser(formUser)),
+});
+
+export default connect(null, mapDispatchToProps)(Signup); 
