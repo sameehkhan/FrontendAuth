50392d75d5a454a0bf574f48823082d8ed71061c
Sameeh <sameehkhan1@gmail.com>
2018-12-07 10:40:01 -0800

    Add liking functionality

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,12 +5,12 @@ import ChirpIndexContainer from './chirps/chirp_index_container';
 import SignUpContainer from './session/signup_container';
 import Home from './home/home';
 import { Route } from 'react-router-dom';
-
+import { AuthRoute, ProtectedRoute } from '../utils/route_utils';
 export default () => (
   <div>
     <Route path="/" component={NavBarContainer} />
     <Route exact path="/" component={Home} />
-    <Route path="/chirps" component={ChirpIndexContainer} />
-    <Route path="/signup" component={SignUpContainer} />
+    <ProtectedRoute path="/chirps" component={ChirpIndexContainer} />
+    <AuthRoute path="/signup" component={SignUpContainer} />
   </div>
 );
